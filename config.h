d3925a5b8d7b30e800e9e3d1ae0f0ed732119fd2
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-10-14 16:35:21 +0200

    Adding customization

--- config.h
+++ config.h
@@ -4,30 +4,45 @@
 #define KERNEL "cat /proc/version | cut -d' ' -f1,3"
 #define CPU "grep 'model name' /proc/cpuinfo | uniq | awk -F: '{print $2}' | sed 's/^ *//'"
 #define GPU "lspci | grep -i vga | awk -F: '{print $3}' | cut -d' ' -f 2,4-8"
-#define COLORS "for C in {30..37}; do echo -en \"[${C}m${C} \"; done; echo;"
+#define COLORS "for C in {40..47}; do echo -en \"[${C}m  \"; done; echo -e \"[0m\";"
+#define TERM "ps -aux | grep -e \" 1836 \" | head -1 | sed \"s/  */ /g\" | cut -d' ' -f11"
 
-const char *const show[][2] = {
-        { "host", "echo ${USER}@$(hostname)" },
-        { "kernel", KERNEL },
-        { "distro", DISTRO },
-        { "de", "echo $XDG_CURRENT_DESKTOP" },
-        { "wm", "echo $XDG_CURRENT_DESKTOP" },
-        { "packages", PACKAGES },
-        { "terminal", "echo $TERM" },
-        { "shell", "echo $SHELL" },
-        { "uptime", UPTIME },
-        { "cpu", CPU },
-        { "gpu", GPU },
-        { "model", "cat /sys/class/dmi/id/product_name" },
-        { "vendor", "cat /sys/class/dmi/id/sys_vendor" },
-        { "colors", COLORS },
-};
-
-#define BLUE "\033[34m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
 #define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+#define DEFAULT "\033[39m"
 #define RESET "\033[0m"
 #define BOLD "\033[1m"
 
-const int label_min_width = 9;
-const char *const lthing = "| " BOLD;
-const char *const sep = GREEN "> " RESET;
+#include "concat.h"
+
+const int name_min_width = 9;
+const int resp_min_width = 41;
+const char *const sep = GREEN " >  " RESET;
+const char *const tthing = "┏" concat55("━") "┓";
+const char *const lthing = "┃ " BOLD;
+const char *const rthing = "┃";
+const char *const bthing = "┗" concat55("━") "┛";
+
+const char *const show[][3] = {
+        { "host", "echo ${USER}@$(hostname)" },
+        { "kernel", KERNEL },
+        { "distro", DISTRO },
+        // { "de", "echo $XDG_CURRENT_DESKTOP" },
+        { "wm", "echo $XDG_CURRENT_DESKTOP" },
+        { "packages", PACKAGES },
+        { "terminal", TERM },
+        { "shell", "basename $SHELL" },
+        { "uptime", UPTIME },
+        { "cpu", CPU },
+        { "gpu", GPU },
+        // { "model", "cat /sys/class/dmi/id/product_name" },
+        // { "vendor", "cat /sys/class/dmi/id/sys_vendor" },
+        /* The third argument is the amount of chars that should not be count */
+        { "colors", COLORS, (char *) ((strlen("[XXm") * 8) + strlen("[0m")) },
+};
--- fetch.c
+++ fetch.c
@@ -1,7 +1,9 @@
-#include "config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
 
 int
 main()
@@ -11,13 +13,26 @@ main()
         size_t entries = show_size / show_entry_size;
 
         size_t i;
+
+        if (tthing) printf("%s\n", tthing);
         for (i = 0; i < entries; i++) {
                 const char *name = show[i][0];
                 const char *cmd = show[i][1];
-                FILE *f = popen(cmd, "r");
+                const char *comp = show[i][2];
+                char buf[1024] = { 0 };
+                char *c;
+                FILE *f;
+
+                f = popen(cmd, "r");
+
+                fgets(buf, sizeof buf, f);
+                if ((c = strchr(buf, '\n'))) *c = 0;
+
                 printf("%s", lthing);
-                printf("%-*s", label_min_width, name);
-                printf("%s%s", sep, fgets((char[1024]) {}, 1024, f));
+                printf("%-*s", name_min_width, name);
+                printf("%s%-*s", sep, resp_min_width + comp, buf);
+                printf("%s\n", rthing);
                 fclose(f);
         }
+        if (bthing) printf("%s\n", bthing);
 }
